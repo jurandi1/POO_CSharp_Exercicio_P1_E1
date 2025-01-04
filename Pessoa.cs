41145dfbbd9b107e48e7003271afa8162a669c56
Jurandi Junior <jurandijr20@gmail.com>
2025-01-04 17:30:45 -0300

    Exercício concluído

---
+++ Pessoa.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_CSharp_Exercicio_P1_E1
+{
+    internal class Pessoa
+    {
+        public string Nome;
+        public int Idade;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,34 @@
-﻿internal class Program
+﻿using POO_CSharp_Exercicio_P1_E1;
+using System.Runtime.ConstrainedExecution;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
+        //EXERCÍCIO 01:
+        //Fazer um programa para ler os dados de duas pessoas, depois mostrar o nome da pessoa mais
+        //velha.
+
+        Pessoa p1 = new Pessoa();
+        Pessoa p2 = new Pessoa();
+        Console.WriteLine("Dados da primeira pessoa:");
+        Console.Write("Nome: ");
+        p1.Nome = Console.ReadLine();
+        Console.Write("Idade: ");
+        p1.Idade = int.Parse(Console.ReadLine());
+        Console.WriteLine("Dados da segunda pessoa:");
+        Console.Write("Nome: ");
+        p2.Nome = Console.ReadLine();
+        Console.Write("Idade: ");
+        p2.Idade = int.Parse(Console.ReadLine());
+        if (p1.Idade > p2.Idade)
+        {
+            Console.WriteLine("Pessoa mais velha: " + p1.Nome);
+        }
+        else
+        {
+            Console.WriteLine("Pessoa mais velha: " + p2.Nome);
+        }
+
     }
 }
